01719da471731418bd452083f7e10dfd8e38d7bd
colin <zhang_colin@163.com>
2024-03-17 17:25:16 +0800

    服务端初始化

---
+++ fullstack-management/src/main/java/com/fullstack/FullStackManagementApplication.java
@@ -0,0 +1,15 @@
+package com.fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangcolin
+ */
+@SpringBootApplication
+public class FullStackManagementApplication {
+    public static void main(String[] args) {
+        // 启动Spring应用
+        SpringApplication.run(FullStackManagementApplication.class, args);
+    }
+}
